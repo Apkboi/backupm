f5e9600c70bac929fb1297ad91bdf7a06db261aa
Victor <>
2024-05-27 13:57:32 +0100

    Pushkit

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -21,6 +21,56 @@ import flutter_callkit_incoming
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
+
+  // Call back from Recent history
+      override func application(_ application: UIApplication,
+      }
+
+      // Handle updated push credentials
+      func pushRegistry(_ registry: PKPushRegistry, didUpdate credentials: PKPushCredentials, for type: PKPushType) {
+      }
+
+      func pushRegistry(_ registry: PKPushRegistry, didInvalidatePushTokenFor type: PKPushType) {
+          print("didInvalidatePushTokenFor")
+          SwiftFlutterCallkitIncomingPlugin.sharedInstance?.setDevicePushTokenVoIP("")
+      }
+
+      // Handle incoming pushes
+      func pushRegistry(_ registry: PKPushRegistry, didReceiveIncomingPushWith payload: PKPushPayload, for type: PKPushType, completion: @escaping () -> Void) {
+      }
+
+
+      // Func Call api for Accept
+      func onAccept(_ call: Call, _ action: CXAnswerCallAction) {
+      }
+
+      // Func Call API for Decline
+      func onDecline(_ call: Call, _ action: CXEndCallAction) {
+      }
+
+      // Func Call API for End
+      func onEnd(_ call: Call, _ action: CXEndCallAction) {
+      }
+
+      // Func Call API for TimeOut
+      func onTimeOut(_ call: Call) {
+      }
+
+      // Func Callback Toggle Audio Session
+      func didActivateAudioSession(_ audioSession: AVAudioSession) {
+      }
+
+      // Func Callback Toggle Audio Session
+      func didDeactivateAudioSession(_ audioSession: AVAudioSession) {
+
+          RTCAudioSession.sharedInstance().audioSessionDidDeactivate(audioSession)
+          RTCAudioSession.sharedInstance().isAudioEnabled = false
+      }
+
+      func performRequest(parameters: [String: Any], completion: @escaping (Result<Any, Error>) -> Void) {
+      }
+
+
 }
 
 
