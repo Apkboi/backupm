9e6bbf25b7d6968c5964c4a29bb139e46d42cab1
Apkboi <abraham.nnu@gmail.com>
2024-06-09 17:37:31 +0100

    Swift Dictionary fix

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -77,7 +77,7 @@ import flutter_callkit_incoming
 
         let data = flutter_callkit_incoming.Data(id: id, nameCaller: nameCaller, handle: handle, type: isVideo ? 1 : 0)
         //set more data
-        data.extra = payload.dictionaryPayload["extra"] as? [String: Any]??[String: Any];
+        data.extra = ["user": "abc@123", "platform": "ios", "webrtc_description_id":payload.dictionaryPayload["extra"]["webrtc_description_id"]
         //data.iconName = ...
         //data.....
         SwiftFlutterCallkitIncomingPlugin.sharedInstance?.showCallkitIncoming(data, fromPushKit: true)
